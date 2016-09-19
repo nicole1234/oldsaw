f8be5f042b01e6911f96b57d210b3e78184d1bc7
nicole1234 <nicoleredmond1@gmail.com>
2016-09-18 21:47:17 -0700

    Added project files.

---
+++ javascript/script.js
@@ -0,0 +1,8 @@
+$(window).load(function() {
+  $('.flexslider').flexslider({
+    animation: "fade",
+    animationSpeed:1400,
+    controlNav: false,
+    slideshowSpeed:2500,
+    directionNav: false  });
+});
